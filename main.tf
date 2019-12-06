e38a571db0900c0335acaf90f299da182f2b6698
Stefan Ziegler <edi.gonzales@gmail.com>
2019-12-06 17:19:40 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+}
+
+variable "floating_ip" {
+}
+
+provider "digitalocean" {}
+
+resource "digitalocean_droplet" "app-srv" {
+    name  = "app-srv-${var.environment}"
+    image = "docker-18-04"
+    region = "fra1"
+    #size = "s-3vcpu-1gb"
+    size = "s-2vcpu-4gb"
+    ssh_keys = [25503420,24397269]
+	user_data = "${file("./user-data.yml")}"
+    monitoring = true
+}
+
+resource "digitalocean_project" "app-srv" {
+    name        = "APP-SRV-${var.environment}"
+    description = "APP-SRV"
+    purpose     = "Web Application"
+    resources   = [digitalocean_droplet.app-srv.urn]
+}
